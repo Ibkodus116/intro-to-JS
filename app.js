86ba1e23687539cd49feff72150f6b81611e760a
Dogged Mind <39561119+Ibkodus116@users.noreply.github.com>
2022-06-23 05:11:56 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,3 @@
+document.getElementById('name').innerHTML = "My Name is Ibukun Odusanya"
+document.getElementById('country').innerHTML = "I'm from Nigeria"
+document.getElementById('height').innerHTML = "My height is 5'4"
